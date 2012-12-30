3e21a3475284c766f4e36c6405d80af7ca1dbefd
Vinod Kurup <vinod@kurup.com>
2012-12-29 21:47:33 -0500

    goto-line shows line-nums

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -37,6 +37,18 @@
 ;; open a shell
 (shell)
 
+;; http://whattheemacsd.com//key-bindings.el-01.html
+(global-set-key [remap goto-line] 'goto-line-with-feedback)
+
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (goto-line (read-number "Goto line: ")))
+    (linum-mode -1)))
+
 ;; autocomplete
 (require 'auto-complete-config)
 (setq ac-dictionary-files (list (concat user-emacs-directory ".dict")))
