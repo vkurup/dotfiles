2194e0e5ae8469ee7d9747847ab8449889a9f0d5
Vinod Kurup <vinod@kurup.com>
2012-12-29 21:45:22 -0500

    Switch from python-mode.el to python.el
    Much simpler, more stable
    I lose some smart autocompletion (finding method signatures, etc), but
    I tend not to use that anyway.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,6 +22,7 @@
                       auto-complete)
   "A list of packages to ensure are installed at launch.")
 
+(require 'python)
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
@@ -37,60 +38,9 @@
 (require 'auto-complete-config)
 (setq ac-dictionary-files (list (concat user-emacs-directory ".dict")))
 (ac-config-default)
-;; hack to fix ac-sources after pycomplete.el breaks it
-(add-hook 'python-mode-hook
-          '(lambda ()
-             (setq ac-sources '(ac-source-pycomplete
-                                ac-source-abbrev
-                                ac-source-dictionary
-                                ac-source-words-in-same-mode-buffers))))
 
-;; python-mode
-(setq py-install-directory (concat esk-user-dir "/python-mode/"))
-(add-to-list 'load-path py-install-directory)
-;; this will show method signatures while typing
-(setq py-set-complete-keymap-p t)
-(setq py-use-current-dir-when-execute-p t)
-(require 'python-mode)
-;; activate the virtualenv where Pymacs is located
-(virtualenv-workon "vkinvest")
-
-(defun load-pycomplete ()
-  "Load and initialize pycomplete."
-  (interactive)
-  (let* ((pyshell (py-choose-shell))
-         (path (getenv "PYTHONPATH")))
-    (setenv "PYTHONPATH" (concat
-                          (expand-file-name py-install-directory) "completion"
-                          (if path (concat path-separator path))))
-    (if (py-install-directory-check)
-        (progn
-          (setenv "PYMACS_PYTHON" (if (string-match "IP" pyshell)
-                                      "python"
-                                    pyshell))
-          (autoload 'pymacs-apply "pymacs")
-          (autoload 'pymacs-call "pymacs")
-          (autoload 'pymacs-eval "pymacs")
-          (autoload 'pymacs-exec "pymacs")
-          (autoload 'pymacs-load "pymacs")
-          (load (concat py-install-directory "completion/pycomplete.el") nil t)
-          (add-hook 'python-mode-hook 'py-complete-initialize))
-      (error "`py-install-directory' not set, see INSTALL"))))
-(eval-after-load 'pymacs '(load-pycomplete))
-
-;; pyflakes flymake integration
-;; http://stackoverflow.com/a/1257306/347942
-(when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "pycheckers" (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pyflakes-init)))
-(add-hook 'python-mode-hook 'flymake-mode)
+;; ;; activate the virtualenv where Pymacs is located
+(virtualenv-workon "moodhacker")
 
 (add-hook 'css-mode-hook 'rainbow-mode)
 
