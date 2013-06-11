4040857b35c8d520ffd1584654a855bdb3e82860
Vinod Kurup <vinod@kurup.com>
2013-06-10 21:51:51 -0400

    Make current txn legible in new ledger mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -235,4 +235,4 @@ Anika's favorite: %^{Anika's favorite}
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ledger-font-xact-highlight-face ((t (:background "black")))))
