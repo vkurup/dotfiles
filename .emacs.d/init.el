32cd5650e8aaa20fcb3887075a5a96dc9b0908f3
Vinod Kurup <vinod@kurup.com>
2013-08-13 21:58:32 -0400

    remove code i don't use

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -161,24 +161,8 @@
 
 ;; (load-theme 'adwaita t)
 (load-theme 'zenburn t)
-;;(load-theme 'solarized-dark t)
 
 (global-set-key [(control x) (control r)] 'esk-sudo-edit)
-
-;; thanks johnw: https://gist.github.com/1198329
-(defun find-grep-in-project (command-args)
-  (interactive
-   (progn
-     (list (read-shell-command "Run find (like this): "
-                               '("git ls-files -z | xargs -0 egrep -nH -e " . 41)
-                               'grep-find-history))))
-  (when command-args
-    (let ((null-device nil)) ; see grep
-      (grep command-args))))
-
-;; octave mode for .m files
-(setq auto-mode-alist
-      (cons '("\\.m$" . octave-mode) auto-mode-alist))
 
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
